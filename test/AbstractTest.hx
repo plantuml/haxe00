70114e7a8fe6e6a9b4389c40e00d2287e8895241
Arnaud Roques <arnaud.roques@arkea.com>
2022-03-21 15:27:42 +0100

    wip

--- test/AbstractTest.hx
+++ test/AbstractTest.hx
@@ -3,7 +3,6 @@ using com.plantuml.utils.StartUtils;
 
 import utest.Test;
 import utest.utils.Print;
-import sys.io.File;
 import utest.Assert;
 
 class AbstractTest extends Test {
@@ -15,28 +14,6 @@ class AbstractTest extends Test {
 		final name = Std.string(this);
 		final path = "test/" + name.replaceAll(".", "/") + ".hx";
 		return path;
-	}
-
-	function foo() {
-		var data = File.getContent(getPath());
-		var headerToRemove = null;
-		var result = null;
-		for (s in data.splitLines()) {
-			final tmp = s.beforeStartUml();
-			if (tmp != null)
-				headerToRemove = tmp;
-			s = s.removeHeader(headerToRemove);
-
-			if (s.isArobaseStartDiagram())
-				result = [];
-
-			if (result != null)
-				result.push(s);
-
-			if (s.isArobaseEndDiagram())
-				return result;
-		}
-		return null;
 	}
 }
 // http://www.unexpected-vortices.com/haxe/brief-tutorial.html
