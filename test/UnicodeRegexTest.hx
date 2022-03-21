d96320e2fc3f66cae335db4c8e3d0641fbe927d1
Arnaud Roques <plantuml@gmail.com>
2022-03-21 23:45:35 +0100

    wip

--- test/UnicodeRegexTest.hx
+++ test/UnicodeRegexTest.hx
@@ -3,6 +3,9 @@ import utest.*;
 #if js
 import js.lib.RegExp;
 #end
+#if python
+import python.lib.Re;
+#end
 using hx.strings.Strings;
 
 class UnicodeRegexTest extends utest.Test {
@@ -12,6 +15,25 @@ class UnicodeRegexTest extends utest.Test {
 		Assert.isTrue(r.match(s));
 	}
 	
+	#if python
+	function testBasicThing1AccentPython() {
+		// https://api.haxe.org/java/util/regex/Pattern.html
+		// https://api.haxe.org/python/lib/Regex.html
+		final s = "&#233;".htmlDecode();
+		Assert.equals("é", s); 
+		Assert.equals(1, s.length8());
+		Assert.equals(1, s.length);
+
+		var re = Re.compile("\\w");
+		Assert.isNull(Re.match(re, ";"));
+		var toto = Re.match(re, s);
+		trace(toto);
+		trace(toto.group());
+
+		Assert.notNull(Re.match(Re.compile("\\p{Letter}", Re.UNICODE), s));
+
+	}
+	#end
 	#if js
 	function testBasicThing1AccentJs() {
 		// https://api.haxe.org/js/lib/RegExp.html
