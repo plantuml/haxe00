3e10155d3bead7c1b483b11447ba8da5ba167ff3
Arnaud Roques <plantuml@gmail.com>
2022-03-21 22:54:23 +0100

    wip

--- test/UnicodeRegexTest.hx
+++ test/UnicodeRegexTest.hx
@@ -1,6 +1,8 @@
-
 import utest.*;
 
+#if js
+import js.lib.RegExp;
+#end
 using hx.strings.Strings;
 
 class UnicodeRegexTest extends utest.Test {
@@ -10,17 +12,34 @@ class UnicodeRegexTest extends utest.Test {
 		Assert.isTrue(r.match(s));
 	}
 	
-	function testBasicThing1Accent() {
-		var r = new EReg("\\w", "i");
+	#if js
+	function testBasicThing1AccentJs() {
+		// https://api.haxe.org/js/lib/RegExp.html
+		var r = new RegExp("\\p{Letter}", "u");
 		final s = "&#233;".htmlDecode();
-		#if !js
-		Assert.equals("é", s); 
-		#end
 		Assert.equals(1, s.length8());
 		Assert.equals(1, s.length);
 
-		Assert.isTrue(r.match(s), "Not a letter");
+		Assert.isTrue(r.test(s), "Not a letter");
+
 	}
+	#else
+	function testBasicThing1Accent() {
+		var r1 = new EReg("\\w", "i");
+		#if java
+		var r2 = new EReg("\\p{L}", "u");
+		#end
+		final s = "&#233;".htmlDecode();
+		Assert.equals("é", s); 
+		Assert.equals(1, s.length8());
+		Assert.equals(1, s.length);
+
+		Assert.isTrue(r1.match(s), "Not a letter");
+		#if java
+		Assert.isTrue(r2.match(s), "Not a letter");
+		#end
+	}
+	#end
 	
 
 }
