1f198d2e8a2db44d03fc71c9b0aecfa473430aed
Arnaud Roques <arnaud.roques@arkea.com>
2022-03-21 15:18:55 +0100

    wip

--- src/com/plantuml/PSystemBuilder.hx
+++ src/com/plantuml/PSystemBuilder.hx
@@ -17,25 +17,20 @@ class PSystemBuilder {
 	}
 
 	public function createPSystem(data:Array<String>):Diagram {
-		trace(factories);
 		if (data[0].isArobaseStartDiagram() == false)
 			throw new haxe.exceptions.NotImplementedException();
-		trace(data.last());
 		if (data.last().isArobaseEndDiagram() == false)
 			throw new haxe.exceptions.NotImplementedException();
 
 		final type = DiagramTypeUtils.getTypeFromArobaseStart(data[0]);
 		data = data.removeFirstAndLast();
-		trace(data);
-
-		trace(type);
 		final lines = new BlocLines(data);
 		for (f in factories.filter(x -> x.getDiagramType() == type)) {
 			final result = f.createSystem(lines);
 			if (result != null)
 				return result;
 		}
-		trace(lines);
+
 		return null;
 	}
 }
--- test/a00/A00Test.hx
+++ test/a00/A00Test.hx
@@ -8,11 +8,6 @@ using hx.strings.Strings;
 
 class A00Test extends AbstractTest {
 	function testExecute() {
-		// final path = getPath();
-		// trace('p1=$path');
-		// Assert.equals("test/a00/A00Test.hx", path);
-		// final diag1 = foo();
-		// trace(diag1);
 		final diag2 = "
 			@startmindmap
 			* Debianなダイアグラム
@@ -29,19 +24,8 @@ class A00Test extends AbstractTest {
 		";
 		final p = new Plantuml();
 		p.addLines(diag2);
-		trace(p.getInternalText());
 		final svg = p.getSvg();
 		trace(svg);
-		// var data = File.getContent(path);
-		// Sys.println(data);
-		// Assert.isTrue(false);
+		Assert.isTrue(svg.length > 0);
 	}
-	/*
-			@Test
-		public void testExecute() throws IOException, InterruptedException {
-			final List<GeneratedImage> r = javaFileReader().getGeneratedImages();
-			AssertJUnit.assertEquals(1, r.size());
-			AssertJUnit.assertTrue("no file", r.get(0).getPngFile().exists());
-			assertImage(r.get(0));
-	}*/
 }
