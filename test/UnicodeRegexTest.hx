2ed41063fa0f97534f3b1b9ff06b304e06dfd288
Arnaud Roques <arnaud.roques@arkea.com>
2022-03-22 09:32:49 +0100

    wip

--- test/UnicodeRegexTest.hx
+++ test/UnicodeRegexTest.hx
@@ -1,49 +1,37 @@
 import utest.*;
-
 #if js
 import js.lib.RegExp;
 #end
 #if python
 import python.lib.Re;
 #end
+
 using hx.strings.Strings;
 
 class UnicodeRegexTest extends utest.Test {
 	function testBasicThing1() {
 		var r = new EReg("\\w", "i");
-		final s = "a";
-		Assert.isTrue(r.match(s));
+		Assert.isTrue(r.match(getLetter()));
 	}
-	
+
 	#if python
 	function testBasicThing1AccentPython() {
 		// https://api.haxe.org/java/util/regex/Pattern.html
 		// https://api.haxe.org/python/lib/Regex.html
-		final s = "&#233;".htmlDecode();
-		Assert.equals("é", s); 
-		Assert.equals(1, s.length8());
-		Assert.equals(1, s.length);
 
-		var re = Re.compile("\\w");
-		Assert.isNull(Re.match(re, ";"));
-		var toto = Re.match(re, s);
-		trace(toto);
-		trace(toto.group());
+		final onlyLetter = Re.compile("[^\\W\\d_]");
 
-		Assert.notNull(Re.match(Re.compile("[^\\W\\d_]", Re.UNICODE), s));
-
+		Assert.notNull(Re.match(Re.compile("[^\\W\\d_]", Re.UNICODE), getLetterAccent()));
+		Assert.isNull(Re.match(Re.compile("[^\\W\\d_]", Re.UNICODE), "4"));
 	}
 	#end
+
 	#if js
 	function testBasicThing1AccentJs() {
 		// https://api.haxe.org/js/lib/RegExp.html
 		var r = new RegExp("\\p{Letter}", "u");
-		final s = "&#233;".htmlDecode();
-		Assert.equals(1, s.length8());
-		Assert.equals(1, s.length);
 
-		Assert.isTrue(r.test(s), "Not a letter");
-
+		Assert.isTrue(r.test(getLetterAccent()), "Not a letter");
 	}
 	#else
 	function testBasicThing1Accent() {
@@ -51,17 +39,35 @@ class UnicodeRegexTest extends utest.Test {
 		#if java
 		var r2 = new EReg("\\p{L}", "u");
 		#end
-		final s = "&#233;".htmlDecode();
-		Assert.equals("é", s); 
-		Assert.equals(1, s.length8());
-		Assert.equals(1, s.length);
 
-		Assert.isTrue(r1.match(s), "Not a letter");
+		Assert.isTrue(r1.match(getLetterAccent()), "Not a letter");
 		#if java
-		Assert.isTrue(r2.match(s), "Not a letter");
+		Assert.isTrue(r2.match(getLetterAccent()), "Not a letter");
 		#end
 	}
 	#end
-	
 
+	function getLetter() {
+		final s = "a";
+		Assert.equals(1, s.length8());
+		Assert.equals(1, s.length);
+		return s;
+	}
+
+	function getNumber() {
+		final s = "2";
+		Assert.equals(1, s.length8());
+		Assert.equals(1, s.length);
+		return s;
+	}
+
+	function getLetterAccent() {
+		final s = "&#233;".htmlDecode();
+		#if !js
+		Assert.equals("é", s);
+		#end
+		Assert.equals(1, s.length8());
+		Assert.equals(1, s.length);
+		return s;
+	}
 }
