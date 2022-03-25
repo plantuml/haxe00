e6ba3340d79a00c5b5f112a127880d1a6838466f
Arnaud Roques <arnaud.roques@arkea.com>
2022-03-25 17:58:08 +0100

    wip

--- src/com/plantuml/error/DiagramCrash.hx
+++ src/com/plantuml/error/DiagramCrash.hx
@@ -1,17 +1,22 @@
 package com.plantuml.error;
 
-import haxe.Exception;
-import com.plantuml.command.BlocLines;
 import com.plantuml.core.Diagram;
 import com.plantuml.cucadiagram.Display;
 import com.plantuml.graphic.FontConfiguration;
 import com.plantuml.ugraphic.UGraphic;
-import com.plantuml.ugraphic.UTranslate;
 import com.plantuml.ugraphic.color.Color;
 import com.plantuml.ugraphic.color.HColor;
+import haxe.CallStack;
+import haxe.Exception;
 
 class DiagramCrash extends Diagram {
-	public function new(e:Exception) {}
+	public function new(e:Exception) {
+		trace(e.message);
+		final st:CallStack = e.stack;
+		for (item in st) {
+			trace('item=$item');
+		}
+	}
 
 	public function exportDiagramNow(ug:UGraphic) {
 		final back:Color = "#000000";
