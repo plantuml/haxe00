3256117293c917d28bac0a4cd6de3faadf86747f
Arnaud Roques <arnaud.roques@arkea.com>
2022-03-30 10:59:54 +0200

    wip

--- runtestverbose.sh
+++ runtestverbose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/opt/jdk1.8.0_102/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/Arnaud.Roques/n/bin:/home/Arnaud.Roques
 export JAVA_HOME=/opt/jdk1.8.0_102
-rm *.py *.out
+rm *.out unit-test.py
 haxe unit-test.hxml
 python3 unit-test.py > unit-test-py.out
 java -jar unit-test-java/MainTest.jar > unit-test-java.out
